c7e3aefc71cf026fb2cb13a4179e9f8351b56829
shiori0208 <shioriuem02@gmail.com>
2025-09-25 23:23:42 +0530

    num pair hashing...

---
+++ Arrays_and_Hashing/Num_Pairs_Target.py
@@ -0,0 +1,13 @@
+#Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        
+        prevMap = {} #val : index 
+
+        for i, n in enumerate(nums):
+            diff = target - n 
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return 
