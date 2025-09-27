a1e55e659ccb69ee345dd7c932a524ef06847a18
shiori0208 <shioriuem02@gmail.com>
2025-09-27 19:39:14 +0530

    learnt about ord function in anagram grouping...

---
+++ 01_Arrays_and_Hashing/Anagram_Grouping_M.py
@@ -0,0 +1,27 @@
+#Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+
+#Input: strs = ["act","pots","tops","cat","stop","hat"]
+#Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+
+
+from collections import defaultdict
+
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        
+        res = defaultdict(list) #mapping character Counts to the list of anagrams
+
+        #for every string in input...
+        for s in strs: 
+            count = [0] * 26 #making an array of all alphabets a....z
+
+            #for every letter in string...
+            for c in s:
+                count[ord(c) - ord("a")] += 1 
+                #ord gives numeric value of letter to place count at right index
+                #eg: c=82 82-80 = 2 (index[2]=1)
+
+            res[tuple(count)].append(s)
+
+        return list(res.values())
