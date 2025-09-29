9224fd0c43400d08a580c9a33cc1106b88c03633
shiori0208 <shioriuem02@gmail.com>
2025-09-29 19:33:53 +0530

    adding i/o examples for all problems...

--- 01_Arrays_and_Hashing/Num_Pairs_Target_E.py
+++ 01_Arrays_and_Hashing/Num_Pairs_Target_E.py
@@ -1,5 +1,9 @@
 #Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
 
+#Input: nums = [3,4,5,6], target = 7
+#Output: [0,1]
+
+
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         
