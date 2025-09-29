c19347d75744f04cce816a0b1cc4b3d57ad11d87
shiori0208 <shioriuem02@gmail.com>
2025-09-29 19:33:04 +0530

    adding i/o examples for all problems...

--- 01_Arrays_and_Hashing/Contains_Duplicates_E.py
+++ 01_Arrays_and_Hashing/Contains_Duplicates_E.py
@@ -1,5 +1,9 @@
 #Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 
+#Input: nums = [1, 2, 3, 3]
+#Output: true
+
+
 class Solution:
     def hasDuplicate(self, nums: list[int]) -> bool:
         hashset = set() #making a hashset in python 
