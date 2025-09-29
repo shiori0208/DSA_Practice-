dec98fe6cf9e382af2c85ef56e197bb92487d4b2
shiori0208 <shioriuem02@gmail.com>
2025-09-29 19:34:50 +0530

    adding i/o examples for all problems...

--- 01_Arrays_and_Hashing/Valid_Anagram_E.py
+++ 01_Arrays_and_Hashing/Valid_Anagram_E.py
@@ -1,5 +1,9 @@
 #Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
 
+#Input: s = "racecar", t = "carrace"
+#Output: true
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
