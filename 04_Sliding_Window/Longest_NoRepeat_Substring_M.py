70ef7fe06d2581667b4d88d55a2e967f18b96c4e
shiori0208 <shioriuem02@gmail.com>
2025-10-05 14:28:48 +0530

    using pointers in sliding window form to find longest non repeating substring...

---
+++ 04_Sliding_Window/Longest_NoRepeat_Substring_M.py
@@ -0,0 +1,20 @@
+#Given a string s, find the length of the longest substring without duplicate characters.
+
+#Input: s = "zxyzxyz"
+#Output: 3
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0 
+        res = 0 
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r-l+1)
+
+        return res 
+
