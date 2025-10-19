8653934aead40755d81b29b295fa0ac905e42c82
shiori0208 <shioriuem02@gmail.com>
2025-10-19 20:47:03 +0530

    using pop logic to make pairs of heights and indexes and calculating final areas using boundaries...

---
+++ 03_Stack/Largest_Rectangle_In_Histogram_H.py
@@ -0,0 +1,23 @@
+#You are given an array of integers heights where heights[i] represents the height of a bar. The width of each bar is 1. Return the area of the largest rectangle that can be formed among the bars. Note: This chart is known as a histogram.
+
+#Input: heights = [1,3,7,4]
+#Output: 9
+
+class Solution:
+    def largestRectangleArea(self, heights: list[int]) -> int:
+        maxArea = 0
+        stack = [] #pair: (index, height)
+
+        for i, h in enumerate(heights):
+            start = i 
+            while stack and stack[-1][1] > h:
+                index, height = stack.pop()
+                maxArea = max(maxArea, height * (i - index))
+                start = index
+            stack.append((start, h))
+
+
+        for i, h in stack:
+            maxArea = max(maxArea, h * (len(heights) - i))
+        return maxArea 
+
