7994ea75961b75e795dfe3eca3a4cd7c815ef2ea
shiori0208 <shioriuem02@gmail.com>
2025-11-05 19:43:48 +0530

    removing nth node from the end by spacing two pointers accordingly as n...

--- 05_Linked_List/Remove_Nth_Node_M.py
+++ 05_Linked_List/Remove_Nth_Node_M.py
@@ -1,3 +1,11 @@
+#You are given the beginning of a linked list head, and an integer n.
+#Remove the nth node from the end of the list and return the beginning of the list.
+
+#Input: head = [1,2,3,4], n = 2
+#Output: [1,2,4]
+
+
+
 # Definition for singly-linked list.
 
 
