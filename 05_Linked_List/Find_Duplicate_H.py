f925204ea5f72bfecf96177f60148b26aceecab1
shiori0208 <shioriuem02@gmail.com>
2025-11-06 23:27:31 +0530

    simply swap children of root and then go down each level and keep repeating the process recursively...

--- 05_Linked_List/Find_Duplicate_H.py
+++ 05_Linked_List/Find_Duplicate_H.py
@@ -1,10 +1,8 @@
-
 #You are given an array of integers nums containing n + 1 integers. Each integer in nums is in the range [1, n] inclusive.
 #Every integer appears exactly once, except for one integer which appears two or more times. Return the integer that appears more than once.
 
 #Input: nums = [1,2,3,2,2]
 #Output: 2
-
 
 
 class Solution:
