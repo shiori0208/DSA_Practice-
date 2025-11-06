7f9f90f30ebd3e28253e56e2a3c2b4452cf9a84e
shiori0208 <shioriuem02@gmail.com>
2025-11-06 23:28:43 +0530

    simply swap children of root and then go down each level and keep repeating the process recursively...

--- 07_Binary_Trees/Inverting_a_tree_E.py
+++ 07_Binary_Trees/Inverting_a_tree_E.py
@@ -1,4 +1,7 @@
 
+#You are given the root of a binary tree root. Invert the binary tree and return its root.
+#Input: root = [1,2,3,4,5,6,7]
+#Output: [1,3,2,7,6,5,4]
 
 
 # Definition for a binary tree node.
