8477fd055d2387c2580b02b21df23ee3986907a8
shiori0208 <shioriuem02@gmail.com>
2025-11-06 23:19:07 +0530

    finding out duplicate in linked list by treating them like index numbers in an array and finding a cycle...

---
+++ 05_Linked_List/Find_Duplicate_H.py
@@ -0,0 +1,27 @@
+
+#You are given an array of integers nums containing n + 1 integers. Each integer in nums is in the range [1, n] inclusive.
+#Every integer appears exactly once, except for one integer which appears two or more times. Return the integer that appears more than once.
+
+#Input: nums = [1,2,3,2,2]
+#Output: 2
+
+
+
+class Solution:
+    def findDuplicate(self, nums: list[int]) -> int:
+        slow, fast = 0, 0
+        
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2=0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow 
+
+#we are using Flyod's concept of fast and slow pointers again in loop
