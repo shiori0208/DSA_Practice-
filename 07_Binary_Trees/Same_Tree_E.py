4f32feae549172c4a9f84f7b60c46b91dd1e1a0f
shiori0208 <shioriuem02@gmail.com>
2025-11-12 11:01:01 +0530

    checking each node recursively to make sure all node values match...

--- 07_Binary_Trees/Same_Tree_E.py
+++ 07_Binary_Trees/Same_Tree_E.py
@@ -4,8 +4,10 @@
 #Output: false
 
 
-
 # Definition for a binary tree node.
+from typing import Optional
+
+
 class TreeNode:
      def __init__(self, val=0, left=None, right=None):
          self.val = val
