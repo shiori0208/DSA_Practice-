8dc535a30f2ddf52d513e2ee9f956938eed1fa35
shiori0208 <shioriuem02@gmail.com>
2025-11-12 15:25:18 +0530

    traversing levels of a binary tree using deque...

--- 07_Binary_Trees/Lowest_Common_Ancestor_M.py
+++ 07_Binary_Trees/Lowest_Common_Ancestor_M.py
@@ -1,3 +1,9 @@
+#Given a binary search tree (BST) where all node values are unique, and two nodes from the tree p and q, return the lowest common ancestor (LCA) of the two nodes.
+
+#Input: root = [5,3,8,1,4,7,9,null,2], p = 3, q = 4
+#Output: 3
+
+
 # Definition for a binary tree node.
 class TreeNode:
      def __init__(self, val=0, left=None, right=None):
