e2a6cf1c62b739565d84c600a6c707629b0d5aa4
shiori0208 <shioriuem02@gmail.com>
2025-11-12 20:59:12 +0530

    printing out the nodes that can only be visible from right side of the tree using BFS...

---
+++ 07_Binary_Trees/Only_Right_View_M.py
@@ -0,0 +1,41 @@
+
+#You are given the root of a binary tree. Return only the values of the nodes that are visible from the right side of the tree, ordered from top to bottom.
+
+#Input: root = [1,2,3]
+#Output: [1,3]
+
+
+
+# Definition for a binary tree node.
+import collections
+from typing import Optional
+
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> list[int]:
+       res = []
+       q = collections.deque([root]) 
+
+       while q:
+
+        rightSide = None
+        qLen = len(q)
+
+        for i in range(qLen):
+            node = q.popleft()
+            if node:
+                rightSide = node 
+                q.append(node.left)
+                q.append(node.right)
+
+        if rightSide:
+            res.append(rightSide.val)
+       return res  
+
+        
