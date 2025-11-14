4e9faeb3190712b166389aeb7674c8a9c6aa3b40
shiori0208 <shioriuem02@gmail.com>
2025-11-14 21:04:50 +0530

    counting good nodes based on dfs recursion...

--- 07_Binary_Trees/Good_Node_Count_M.py
+++ 07_Binary_Trees/Good_Node_Count_M.py
@@ -1,3 +1,9 @@
+#Within a binary tree, a node x is considered good if the path from the root of the tree to the node x contains no nodes with a value greater than the value of node x
+#Given the root of a binary tree root, return the number of good nodes within the tree.
+
+#Input: root = [1,2,-1,3,4]
+#Output: 4
+
 
 # Definition for a binary tree node.
 class TreeNode:
