a3b483568cc8a10f28198d1a151a5508c3e85d11
shiori0208 <shioriuem02@gmail.com>
2025-12-11 23:16:57 +0530

    implementing prefix tree using functions and tree nodes...

---
+++ 08_Tries/Prefix_Tree_Implement.py
@@ -0,0 +1,8 @@
+#A prefix tree (also known as a trie) is a tree data structure used to efficiently store and retrieve keys in a set of strings. Some applications of this data structure include auto-complete and spell checker systems.
+
+#Input: 
+#["Trie", "insert", "dog", "search", "dog", "search", "do", "startsWith", "do", "insert", "do", "search", "do"]
+
+#Output:
+#[null, null, true, false, true, null, true]
+
