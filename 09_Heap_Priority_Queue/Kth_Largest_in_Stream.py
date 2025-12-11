5bfea22cb613d6519b53552a61b09b4f0e01dca1
shiori0208 <shioriuem02@gmail.com>
2025-12-11 23:20:03 +0530

    using minheap and python inbuilt functions in order to use heap to sort array and reduce time complexity...

---
+++ 09_Heap_Priority_Queue/Kth_Largest_in_Stream.py
@@ -0,0 +1,7 @@
+#Design a class to find the kth largest integer in a stream of values, including duplicates. E.g. the 2nd largest from [1, 2, 3, 3] is 3. The stream is not necessarily sorted.
+
+#Input:
+#["KthLargest", [3, [1, 2, 3, 3]], "add", [3], "add", [5], "add", [6], "add", [7], "add", [8]]
+
+#Output:
+#[null, 3, 3, 3, 5, 6]
