67fded632320d4f1b673a698d4bc0c10f566b3f7
shiori0208 <shioriuem02@gmail.com>
2025-12-15 23:27:28 +0530

    using distance formula and minheap to pop k times and get the required list...

---
+++ 09_Heap_Priority_Queue/K_Closest_Points_to_Origins_M.py
@@ -0,0 +1,4 @@
+#You are given an 2-D array points where points[i] = [xi, yi] represents the coordinates of a point on an X-Y axis plane. You are also given an integer k.
+
+#Return the k closest points to the origin (0, 0).
+
