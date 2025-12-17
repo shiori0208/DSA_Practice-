be92240f7e190986a4332330c7344d8d51ba6520
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-17 23:53:14 +0530

    Add comments for task scheduler functionality
    Added comments to explain the function of the code.

--- 09_Heap_Priority_Queue/Task_Scheduler_M.py
+++ 09_Heap_Priority_Queue/Task_Scheduler_M.py
@@ -1,2 +1,5 @@
 #You are given an array of CPU tasks tasks, where tasks[i] is an uppercase english character from A to Z. You are also given an integer n.
 #Return the minimum number of CPU cycles required to complete all tasks.
+
+#Input: tasks = ["X","X","Y","Y"], n = 2
+#Output: 5
