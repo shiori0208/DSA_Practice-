936bd7b4648f6cb9dcec47b9066cf9fe3f0f46db
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-17 23:57:19 +0530

    Add problem description for Combination Sum
    Add a description for the Combination Sum problem.

---
+++ 10_Backtracking/Combination_Sum_M.py
@@ -0,0 +1,1 @@
+#You are given an array of distinct integers nums and a target integer target. Your task is to return a list of all unique combinations of nums where the chosen numbers sum to target.
