d99eb402ffee208415250cc07ed10cb036ba8e3e
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-17 23:54:19 +0530

    Add input/output examples for Design Twitter
    Added input and output examples for the Twitter implementation.

--- 09_Heap_Priority_Queue/Design_Twitter_M.py
+++ 09_Heap_Priority_Queue/Design_Twitter_M.py
@@ -1,1 +1,7 @@
 #Implement a simplified version of Twitter which allows users to post tweets, follow/unfollow each other, and view the 10 most recent tweets within their own news feed.
+
+#Input:
+#["Twitter", "postTweet", [1, 10], "postTweet", [2, 20], "getNewsFeed", [1], "getNewsFeed", [2], "follow", [1, 2], "getNewsFeed", [1], "getNewsFeed", [2], "unfollow", [1, 2], "getNewsFeed", [1]]
+
+#Output:
+#[null, null, null, [10], [20], null, [20, 10], [20], null, [10]]
