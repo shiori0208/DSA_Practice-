9a764a7a87d0bcf0635a538347e1188af2705fed
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-19 23:56:05 +0530

    using bit shift operation to go through bitwise
    Implement Hamming weight calculation for an integer.

--- 11_Bit_Manipulation/Hamming_Weight_E.py
+++ 11_Bit_Manipulation/Hamming_Weight_E.py
@@ -1,2 +1,10 @@
 #You are given an unsigned integer n. Return the number of 1 bits in its binary representation.
 #You may assume n is a non-negative integer which fits within 32-bits.
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            res += n%2
+            n = n >> 1 
+        return res 
