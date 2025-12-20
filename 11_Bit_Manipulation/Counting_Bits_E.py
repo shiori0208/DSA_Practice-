636f5b6696a5c73f2717bfdf6b3e210391f84875
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-20 22:46:48 +0530

    Add comments for clarity on Counting_Bits_E.py
    Added comments to explain the function's purpose and example usage.

--- 11_Bit_Manipulation/Counting_Bits_E.py
+++ 11_Bit_Manipulation/Counting_Bits_E.py
@@ -1,3 +1,8 @@
 #Given an integer n, count the number of 1's in the binary representation of every number in the range [0, n].
 #Return an array output where output[i] is the number of 1's in the binary representation of i.
 
+#Input: n = 4
+#Output: [0,1,1,2,1]
+
+
+
