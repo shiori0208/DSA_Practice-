0cfe84cb5666c39628ced07d59484684b5a63932
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-20 22:48:39 +0530

    Add examples for Hamming Weight function
    Added input and output examples as comments.

--- 11_Bit_Manipulation/Hamming_Weight_E.py
+++ 11_Bit_Manipulation/Hamming_Weight_E.py
@@ -1,6 +1,9 @@
 #You are given an unsigned integer n. Return the number of 1 bits in its binary representation.
 #You may assume n is a non-negative integer which fits within 32-bits.
 
+#Input: n = 00000000000000000000000000010111
+#Output: 4
+
 class Solution:
     def hammingWeight(self, n: int) -> int:
         res = 0
