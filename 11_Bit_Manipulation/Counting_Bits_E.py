5485a6d8f1669accc7d3651db151ea1c6db849e1
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-20 22:44:57 +0530

    Implement Counting Bits function
    Add a function to count the number of 1's in binary representation for numbers from 0 to n.

---
+++ 11_Bit_Manipulation/Counting_Bits_E.py
@@ -0,0 +1,3 @@
+#Given an integer n, count the number of 1's in the binary representation of every number in the range [0, n].
+#Return an array output where output[i] is the number of 1's in the binary representation of i.
+
