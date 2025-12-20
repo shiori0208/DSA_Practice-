128d0286d6ee1d4039e0b89ccc0062d469eda2c3
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-20 22:47:05 +0530

    Add countBits method to Solution class
    Implement countBits method to count set bits.

--- 11_Bit_Manipulation/Counting_Bits_E.py
+++ 11_Bit_Manipulation/Counting_Bits_E.py
@@ -4,5 +4,17 @@
 #Input: n = 4
 #Output: [0,1,1,2,1]
 
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n+1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp 
+
+
 
 
