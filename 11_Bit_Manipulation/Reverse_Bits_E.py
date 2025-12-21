b8d9bda0a478759b7b83474c82618b24590420e0
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-21 22:34:43 +0530

    Implement reverseBits function to reverse bits of n

--- 11_Bit_Manipulation/Reverse_Bits_E.py
+++ 11_Bit_Manipulation/Reverse_Bits_E.py
@@ -2,3 +2,12 @@
 
 #Input: n = 00000000000000000000000000010101
 #Output:    2818572288 (10101000000000000000000000000000)
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0 
+
+        for i in range(32):
+            bit = (n >> i) & 1 
+            res = res | (bit << (31 - i))
+        return res 
