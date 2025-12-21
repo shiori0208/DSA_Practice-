97db1a6821590c5e15922d21b2f7ef40ced705fe
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-21 22:34:21 +0530

    Add comments for clarity on bit reversal function
    Added comments to explain the function's purpose and input/output.

--- 11_Bit_Manipulation/Reverse_Bits_E.py
+++ 11_Bit_Manipulation/Reverse_Bits_E.py
@@ -1,1 +1,4 @@
 #Given a 32-bit unsigned integer n, reverse the bits of the binary representation of n and return the result.
+
+#Input: n = 00000000000000000000000000010101
+#Output:    2818572288 (10101000000000000000000000000000)
