01bce4fa5d591c4b93a8752a5bf7538132d186a0
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-24 21:11:11 +0530

    Add merge function to merge overlapping intervals
    Implement merge function to combine overlapping intervals.

--- 12_Intervals/Merge_Intervals.py
+++ 12_Intervals/Merge_Intervals.py
@@ -3,3 +3,18 @@
 
 #Input: intervals = [[1,3],[1,5],[6,7]]
 #Output: [[1,5],[6,7]]
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key = lambda i : i[0])
+        res = [intervals[0]]
+
+        for start, end in intervals[1:]:
+            lastEnd = res[-1][1]
+
+            if start <= lastEnd:
+                res[-1][1] = max(lastEnd, end)
+            else:
+                res.append([start, end])
+            
+        return res
