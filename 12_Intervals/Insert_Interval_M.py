e3d9b674ebedd21cbade729f09fd3f535c4ded97
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-24 21:08:26 +0530

    Add Insert_Interval_M.py file

--- 12_Intervals/Insert_Interval_M.py
+++ 12_Intervals/Insert_Interval_M.py
@@ -6,3 +6,19 @@
 
 #Input: intervals = [[1,3],[4,6]], newInterval = [2,5]
 #Output: [[1,6]]
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+
+        res.append(newInterval)
+        return res
