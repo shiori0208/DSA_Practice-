61a397fb4d69861835b889be7d1433df476b1891
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-24 21:13:52 +0530

    Add meeting room conflict checking functionality
    Implement function to check meeting schedule conflicts.

---
+++ 12_Intervals/Meetuing_Room.py
@@ -0,0 +1,5 @@
+#Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] (start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
+#Note: (0,8),(8,10) is not considered a conflict at 8
+
+#Input: intervals = [(0,30),(5,10),(15,20)]
+#Output: false
