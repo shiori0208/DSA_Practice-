0290df813ae4c986e2720a268391ef92f4709708
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2025-12-24 21:10:46 +0530

    Implement merge intervals function
    Added a function to merge overlapping intervals.

---
+++ 12_Intervals/Merge_Intervals.py
@@ -0,0 +1,5 @@
+#Given an array of intervals where intervals[i] = [start_i, end_i], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+#You may return the answer in any order.
+
+#Input: intervals = [[1,3],[1,5],[6,7]]
+#Output: [[1,5],[6,7]]
