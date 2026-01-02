5161903444f360c798362835de1d3f3e8a3f0d7a
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-02 23:00:56 +0530

    Add example input and output comments
    Added example input and output for clarity.

--- 13_Greedy/Maximum Subarray_M.py
+++ 13_Greedy/Maximum Subarray_M.py
@@ -1,3 +1,5 @@
 #Given an array of integers nums, find the subarray with the largest sum and return the sum.
 #A subarray is a contiguous non-empty sequence of elements within an array.
 
+#Input: nums = [2,-3,4,-2,2,1,-1,4]
+#Output: 8
