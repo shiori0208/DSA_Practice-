6880718f9eb746c1c32132db3292d3b00c623970
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-02 23:02:51 +0530

    Add problem description for Jump Game
    Added a docstring explaining the Jump Game problem.

---
+++ 13_Greedy/Jump Game_M.py
@@ -0,0 +1,2 @@
+#You are given an integer array nums where each element nums[i] indicates your maximum jump length at that position.
+#Return true if you can reach the last index starting from index 0, or false otherwise.
