9f4c405d3414d4294a538a9d16a38149f9887154
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-02 23:00:25 +0530

    Enhance comments for Maximum Subarray_M.py
    Added a detailed description of the Maximum Subarray problem.

--- 13_Greedy/Maximum Subarray_M.py
+++ 13_Greedy/Maximum Subarray_M.py
@@ -1,2 +1,3 @@
-#Maximum Subarray
+#Given an array of integers nums, find the subarray with the largest sum and return the sum.
+#A subarray is a contiguous non-empty sequence of elements within an array.
 
