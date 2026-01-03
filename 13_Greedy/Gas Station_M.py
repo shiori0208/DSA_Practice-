b1effcdee4855339dafdd6498c1cc11d5f326634
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-03 22:45:34 +0530

    Add problem description for Gas Station_M.py
    Added a description for the Gas Station problem.

---
+++ 13_Greedy/Gas Station_M.py
@@ -0,0 +1,6 @@
+#There are n gas stations along a circular route. You are given two integer arrays gas and cost where:
+#gas[i] is the amount of gas at the ith station.
+#cost[i] is the amount of gas needed to travel from the ith station to the (i + 1)th station. (The last station is connected to the first station)
+
+#You have a car that can store an unlimited amount of gas, but you begin the journey with an empty tank at one of the gas stations.
+#Return the starting gas station's index such that you can travel around the circuit once in the clockwise direction. If it's impossible, then return -1.
