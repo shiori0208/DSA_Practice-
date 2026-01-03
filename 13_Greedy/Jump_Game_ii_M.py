c00c97bb67fa106882334c753042f63db8f41d05
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-03 22:43:00 +0530

    Add problem statement comment to Jump_Game_ii_M.py
    Added a comment explaining the problem statement for Jump Game II.

---
+++ 13_Greedy/Jump_Game_ii_M.py
@@ -0,0 +1,3 @@
+#You are given an array of integers nums, where nums[i] represents the maximum length of a jump towards the right from index i. For example, if you are at nums[i], you can jump to any index i + j where:
+#Return the minimum number of jumps to reach the last position in the array (index nums.length - 1). You may assume there is always a valid answer.
+
