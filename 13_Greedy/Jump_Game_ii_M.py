7b154e4d497abe2c94283a5ac47432e7108d6dce
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-03 22:43:46 +0530

    Add input/output examples to Jump_Game_ii_M.py
    Added input and output examples for clarity.

--- 13_Greedy/Jump_Game_ii_M.py
+++ 13_Greedy/Jump_Game_ii_M.py
@@ -1,3 +1,6 @@
 #You are given an array of integers nums, where nums[i] represents the maximum length of a jump towards the right from index i. For example, if you are at nums[i], you can jump to any index i + j where:
 #Return the minimum number of jumps to reach the last position in the array (index nums.length - 1). You may assume there is always a valid answer.
 
+#Input: nums = [2,4,1,1,1,1]
+#Output: 2
+
