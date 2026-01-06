474a0a09a40ca4efdae8fe2a3c09f3bd0d9290d6
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-06 23:28:34 +0530

    Create Hand of Straights_M.py with problem description
    Add initial implementation for Hand of Straights problem.

---
+++ 13_Greedy/Hand of Straights_M.py
@@ -0,0 +1,6 @@
+#You are given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize.
+#You want to rearrange the cards into groups so that each group is of size groupSize, and card values are consecutively increasing by 1.
+
+#Input: hand = [1,2,4,2,3,5,3,4], groupSize = 4
+#Output: true
+
