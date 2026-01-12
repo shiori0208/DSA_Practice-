f673dfffdf5172ac429eb0d9ec997db0a6d44c6f
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-12 19:09:54 +0530

    Create WordDictionary class for word management
    Implement WordDictionary class with addWord and search methods.

---
+++ 08_Tries/Add_Search_M.py
@@ -0,0 +1,6 @@
+#Design a data structure that supports adding new words and searching for existing words.
+
+#Implement the WordDictionary class:
+
+#void addWord(word) Adds word to the data structure.
+#bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
