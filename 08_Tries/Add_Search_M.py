63c67d28e13e39ff7929d9837cfa2a4e2bca5f08
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-12 19:10:47 +0530

    Add examples for WordDictionary class usage
    Added input and output examples for the WordDictionary class.

--- 08_Tries/Add_Search_M.py
+++ 08_Tries/Add_Search_M.py
@@ -4,3 +4,6 @@
 
 #void addWord(word) Adds word to the data structure.
 #bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise. word may contain dots '.' where dots can be matched with any letter.
+
+#Input: ["WordDictionary","addWord","addWord","addWord","search","search","search","search"] [[],["day"],["bay"],["may"],["say"],["day"],[".ay"],["b.."]]
+#Output:[null, null, null, null, false, true, true, true]
