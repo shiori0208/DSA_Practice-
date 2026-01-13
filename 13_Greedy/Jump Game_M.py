3718ffd0e7618f2b47270b4d95ea255e07e6508e
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-13 22:51:26 +0530

    Add examples for Jump Game problem
    Added input and output examples for clarity.

--- 13_Greedy/Jump Game_M.py
+++ 13_Greedy/Jump Game_M.py
@@ -1,5 +1,8 @@
 #You are given an integer array nums where each element nums[i] indicates your maximum jump length at that position.
 #Return true if you can reach the last index starting from index 0, or false otherwise.
 
+#Input: nums = [1,2,0,1,0]
+#Output: true
+
 
        
