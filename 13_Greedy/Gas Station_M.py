a48eb0ab6e765de04e7cfdeb7ccea7ca99170bb5
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-14 01:50:33 +0530

    Add canCompleteCircuit method to Solution class
    Implement canCompleteCircuit method to determine if a circuit can be completed with given gas and cost arrays.

--- 13_Greedy/Gas Station_M.py
+++ 13_Greedy/Gas Station_M.py
@@ -8,5 +8,23 @@
 #Input: gas = [1,2,3,4], cost = [2,2,4,1]
 #Output: 3
 
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        total = 0
+        res = 0
+
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0:
+                total = 0
+                res = i + 1 
+
+        return res
+        
+
 
         
