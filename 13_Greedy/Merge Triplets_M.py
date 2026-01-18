9f8b6969ab38214a90ea4448b646c0d77f159d5f
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-18 22:29:12 +0530

    Add function description for Merge Triplets_M.py

---
+++ 13_Greedy/Merge Triplets_M.py
@@ -0,0 +1,4 @@
+#You are given a 2D array of integers triplets, where triplets[i] = [ai, bi, ci] represents the ith triplet. You are also given an array of integers target = [x, y, z] which is the triplet we want to obtain.
+#Choose two different triplets triplets[i] and triplets[j] and update triplets[j] to become [max(ai, aj), max(bi, bj), max(ci, cj)].
+#E.g. if triplets[i] = [1, 3, 1] and triplets[j] = [2, 1, 2], triplets[j] will be updated to [max(1, 2), max(3, 1), max(1, 2)] = [2, 3, 2].
+
