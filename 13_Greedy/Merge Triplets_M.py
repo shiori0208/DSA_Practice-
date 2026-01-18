d411a15112b948a872464c70bf49761cd6afaeb7
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-18 22:29:55 +0530

    Add example input and output for Merge Triplets
    Added input and output examples for clarity.

--- 13_Greedy/Merge Triplets_M.py
+++ 13_Greedy/Merge Triplets_M.py
@@ -2,3 +2,8 @@
 #Choose two different triplets triplets[i] and triplets[j] and update triplets[j] to become [max(ai, aj), max(bi, bj), max(ci, cj)].
 #E.g. if triplets[i] = [1, 3, 1] and triplets[j] = [2, 1, 2], triplets[j] will be updated to [max(1, 2), max(3, 1), max(1, 2)] = [2, 3, 2].
 
+#Input: triplets = [[1,2,3],[7,1,1]], target = [7,2,3]
+#Output: true
+
+
+
