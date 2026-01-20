a506928a2a2b82251e462015473301768fc78f39
Shiori Roy <68960689+shiori0208@users.noreply.github.com>
2026-01-20 11:41:00 +0530

    Add comments for partition labels problem
    Added initial comments explaining the problem statement for partitioning labels.

---
+++ 13_Greedy/Partition Labels_M.py
@@ -0,0 +1,5 @@
+#You are given a string s consisting of lowercase english letters.
+#We want to split the string into as many substrings as possible, while ensuring that each letter appears in at most one substring.
+
+
+
